be5a8028b07eeeabc6f7df16bf1f7b91ac337ae2
colesegura <cwsegura@crimson.ua.edu>
2023-07-19 20:25:35 -0500

    Fix ESLint warning

--- PodcastSearch/ClientApp/src/components/Episode.js
+++ PodcastSearch/ClientApp/src/components/Episode.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Episode.css';
 
 function Episode({ episode }) {
-  const { id } = useParams();
-
-  return (
-    <div className="episode-container">
-      <h2>{episode.title}</h2>
-      <p>{episode.transcript}</p>
-      <p><strong>Podcast:</strong> {episode.podcast}</p>
-      <p><strong>Date:</strong> {episode.date}</p>
-      <p><strong>Spotify:</strong> <a href={episode.spotify} target="_blank" rel="noreferrer">Listen on Spotify</a></p>
-      <p><strong>Apple:</strong> <a href={episode.apple} target="_blank" rel="noreferrer">Listen on Apple Podcasts</a></p>
-      <p><strong>YouTube:</strong> <a href={episode.youtube} target="_blank" rel="noreferrer">Watch on YouTube</a></p>
-      <Link to="/results">Back to Results</Link>
-    </div>
-  );
+    return (
+        <div className="episode-container">
+            <h2>{episode.title}</h2>
+            <p>{episode.transcript}</p>
+            <p><strong>Podcast:</strong> {episode.podcast}</p>
+            <p><strong>Date:</strong> {episode.date}</p>
+            <p><strong>Spotify:</strong> <a href={episode.spotify} target="_blank" rel="noreferrer">Listen on Spotify</a></p>
+            <p><strong>Apple:</strong> <a href={episode.apple} target="_blank" rel="noreferrer">Listen on Apple Podcasts</a></p>
+            <p><strong>YouTube:</strong> <a href={episode.youtube} target="_blank" rel="noreferrer">Watch on YouTube</a></p>
+            <Link to="/results">Back to Results</Link>
+        </div>
+    );
 }
 
 export default Episode;
